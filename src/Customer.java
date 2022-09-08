d086de3069469adab2b5a0b0f675fe3d3564ebcb
JeremyDuncan <jduncan46@my.devry.edu>
2022-09-08 09:40:09 -0400

    added constructors and behaviors

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,6 @@
+
+import java.text.DecimalFormat;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,6 +18,44 @@ public class Customer {
     private double length;
     private double width;
     private double totalCost;
+
+    // Constructors
+    public Customer() {
+        customerID = 0;
+        name = "N/A";
+        address = "N/A";
+        yardType = "N/A";
+        length = 0.0;
+        width = 0.0;
+        totalCost = 0.0;
+    }
+
+    public Customer(int customerID, String name, String address, String yardType, double length, double width, double totalCost) {
+        this.customerID = customerID;
+        this.name = name;
+        this.address = address;
+        this.yardType = yardType;
+        this.length = length;
+        this.width = width;
+        this.totalCost = totalCost;
+    }
+
+    // Behaviors
+    @Override
+    public String toString() {
+        return name; // Only show customer's name
+    }
     
-    
+    public String getDetails() {
+        DecimalFormat fmt = new DecimalFormat( "$#,##0.00" );
+        String output = name + "\n";
+        output += address + "\n";
+        output += "Type: " + yardType + "\n";
+        output += "Width: " + width + "\n";
+        output += "Length: " + length + "\n";
+        output += "Total Cost: " + fmt.format(totalCost); //currency
+
+        return output;
+    }
+   
 }
