86c429a332b590b5a204f910c69e207785da5d81
JeremyDuncan <jduncan46@my.devry.edu>
2022-09-08 09:28:09 -0400

    created class Customer

---
+++ src/Customer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeremy Duncan
+ */
+public class Customer {
+    private int customerID;
+    private String name;
+    private String address;
+    private String yardType;
+    private double length;
+    private double width;
+    private double totalCost;
+    
+    
+}
