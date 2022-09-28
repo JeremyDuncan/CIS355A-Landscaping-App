3bcddbd55a548b4950a3f44d8ec435e837a60446
JeremyDuncan <jduncan46@my.devry.edu>
2022-09-28 08:30:58 -0400

    added IO class

---
+++ src/DataIO.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeremy Duncan
+ */
+public class DataIO {
+   //behaviors
+    public void add(Customer cust) {
+        
+    }
+    public void delete(int customerID){
+        
+    }
+    public ArrayList<Customer> getList() {
+        return null;
+    }
+    
+}
